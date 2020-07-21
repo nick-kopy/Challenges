36bfb9eb763fb68238052849a12c83ee729ab70b
Nicholas Kopystynsky <60525092+nick-kopy@users.noreply.github.com>
2020-07-20 18:52:20 -0700

    Update prime.py
    - Added docstring
    - Cleaned up code for readability
    - Merged scattered code into one function

--- prime.py
+++ prime.py
@@ -1,52 +1,43 @@
-#really basic prime number counter
-#input integer, tells you how many primes lead up to that
+def primes_below(target):
+    '''Returns all prime numbers leading up to and including a given integer.
+    
+    Arguments:
+        target (int): the target integer (limited to 10,000)
+    
+    Returns:
+        (list): all primes leading up to the target
+    '''
 
-#input
-while True:
-    target = input('Please enter an integer above 1: ')
     try:
         int(target)
     except:
-        continue
-    target = int(target)
+        print('Please use an integer above 1')
+        return None
+
     if target > 10000:
-        print('Ok, slow down there buckaroo...')
-        continue
-    elif target < 2:
-        continue
-    else:
-        break
+        print('Slow down, this is a simple function!')
+        return None
+    
+    # Makes a list of all integers from 2 to the target
+    plist = list(range(2, target + 1))
 
-#make list 0-input (for loops take too much processing power?)
-#is a list the best?
-plist = list()
-lindex = 2
-while (lindex < target + 1):
-    plist.append(lindex)
-    lindex += 1
-#print(plist)
+    index = 0
 
-def getmultiples(v):
-    #returns a list of multiples for plist specifically, takes argument as index
-    mlist = list() #list of multiples
-    m0 = plist[v] #m0 is our target we want multiples of
-    m = m0 * 2 #first multiple
-    while m < target + 1:
-        mlist.append(m) 
-        m = m + m0
-    return mlist #should return empty list when no multiples between v and target
+    # Finds multiples of each item in the list and
+    # knocks those multiples out of the prime list
+    while True:
+        
+        multilist = list()
+        m = plist[index] * 2
+        
+        while m < target + 1:
+            multilist.append(m)
+            m += plist[index]
+        
+        plist = [y for y in plist if y not in multilist]
+        index += 1
 
-#loop to take multiples out of our plist
-#doesn't take multiples of things already taken out (eg no multilist for 4 or 10), effecient!
-index = 0
-while True:
-    if index >= len(plist):
-        break
-    multilist = getmultiples(index)
-    #is it better to skip this subtraction if multilist is empty?
-    #I stole this line from the internet, I don't actually get it
-    plist = [y for y in plist if y not in multilist]
-    index += 1
-
-print(plist)
-print(index)
+        if index >= len(plist):
+            break
+    
+    return plist
