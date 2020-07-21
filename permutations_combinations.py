356444fe7eefda36d5ef9f4c8a3db10a25300193
Nicholas Kopystynsky <60525092+nick-kopy@users.noreply.github.com>
2020-07-21 12:14:27 -0700

    Update permutations_combinations.py
    - Made code into one function
    - Cleaned up for readability
    - Added docstring

--- permutations_combinations.py
+++ permutations_combinations.py
@@ -1,46 +1,39 @@
-'''
-calculates the number of possible permutations or combinations
-'''
-
 from math import factorial
 
-x = input('whole set? \n')
+def ncr_npr(n, r, ordered = 'c'):
+    '''Calculates the number of possible combinations or permutations.
 
-y = input('choosing how many? \n')
+    For more effecient / diverse tools see itertools library,
+    this is just a coding exercise.
 
-try:
-    int(x)
-    int(y)
-except:
-    print('please use positive integers')
-    raise SystemExit
+    Arguments:
+        n (int): population, entire set from which to draw from
+        r (int): how many will be chosen
+        type (str): 'c' for combinations (non-ordered)
+                    or 'p' for permutations (ordered)
+    Returns:
+        (int): number of possible combinations or permutations
+        (None): in the event of an error
+    '''
 
-x = int(x)
-y = int(y)
+    # Conditions that don't need an equation
+    try:
+        int(n)
+        int(r)
+    except:
+        return None
 
-if x <= 0 or y <= 0:
-    print('please use positive integers')
-    raise SystemExit
-
-if y > x:
-    x, y = y, x
-    print("I think you got those backwards, don't worry I know what you mean")
-
-t = input('permutaiton or combination? type "p" or "c"? \n')
-
-def permu(n, r):
-    if n == r:
+    if n <= 0 or r <= 0 or r > n:
+        return None
+    elif n == r and ordered == 'p':
         return factorial(n)
-    return int(factorial(n) / factorial(n-r))
-
-def combi(n, r):
-    if n == r:
+    elif n == r and ordered == 'c':
         return 1
-    return int(factorial(n) / (factorial(n-r) * factorial(r)))
 
-if t == 'c':
-    print(combi(x, y))
-elif t == 'p':
-    print(permu(x, y))
-else:
-    print('please just type one letter "p" or "c"')
+    # Permutation and combination equations
+    if ordered == 'p':
+        return int(factorial(n) / factorial(n-r))
+    elif ordered == 'c':
+        return int(factorial(n) / (factorial(n-r) * factorial(r)))
+    else:
+        return None
